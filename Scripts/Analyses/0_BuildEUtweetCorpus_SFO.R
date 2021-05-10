938eb9ce210647315f938fb4f37a36fa10916a7d
SinaOzdemir <sina.ozdemir@ntnu.no>
2021-05-10 17:15:49 +0100

    Changed paths and variable names.

--- Scripts/Analyses/0_BuildEUtweetCorpus_SFO.R
+++ Scripts/Analyses/0_BuildEUtweetCorpus_SFO.R
@@ -7,8 +7,8 @@
 
 # Packages
 library(tidyverse)
-
-
+library(lubridate)#makes working with date columns extremely easy.
+library(Hmisc)
 # Get indvidual files ####
 
 # Containing tweet text and metadata from individual accounts
@@ -20,7 +20,9 @@ library(tidyverse)
 
 # NOTE: 117 files - correct? 
 #Yep, 117 accounts because the Council of the EU and European Council merged their twitter accounts
-data.path<- paste0(getwd(),"/data/EU/rds_clean/")
+#now 115 accounts...
+subject_matter<-c("EU","IO","UK","TWT")
+data.path<- paste0(getwd(),"/data/",subject_matter[2],"/rds_clean/")
 
 files <- list.files(path = data.path, pattern = "*.RDS",full.names = T)
 
@@ -30,26 +32,60 @@ files <- list.files(path = data.path, pattern = "*.RDS",full.names = T)
 
 #get a sample data to select necessary columns
 
-sample_data<-readRDS(data.path[10])
+sample_data<-readRDS(files[9])
+#there in total 49 variables in a df
+#Not all of them are necessary,
+#eleminating them early on make it easier
+#Otherwise the data is too big to read into memory
+analysis_vars<- c("tweet_id",
+                  "is_retweet",
+                  "retweet_status_id",
+                  "is_reply",
+                  "reply_to_status_id",
+                  "is_quote",
+                  "quoted_status_id",
+                  "contains_media",
+                  "tweet_mentioned_username",
+                  "tweet_hashtags",
+                  "tweet_created_at",
+                  "tweet_text",
+                  "tweet_lang",
+                  "tweet_public_metrics.retweet_count",
+                  "tweet_public_metrics.reply_count",
+                  "tweet_public_metrics.like_count",
+                  "tweet_public_metrics.quote_count",
+                  "author_id",
+                  "user_username",
+                  "user_name",
+                  "user_description",
+                  "user_created_at",
+                  "user_public_metrics.followers_count",
+                  "user_public_metrics.following_count",
+                  "user_public_metrics.tweet_count",
+                  "user_public_metrics.listed_count")
+
+
+
 
 # All files should be identically structured
 
 # Target DF
-corpus <- data.frame()
-
-for (i in 1:length(files)) {
-  
-  # Progress
-  print(i)
-  
-  # Read file
-  current <- read_rds(files[i])
-  
-  # Appended to target DF
-  corpus <- rbind(corpus, current)
-}
-
-rm(current) # Remove last instance 
+# corpus <- data.frame()
+# 
+# 
+# for (i in 1:length(files)) {
+#   
+#   # Progress
+#   print(i)
+#   
+#   # Read file
+#   current <- read_rds(files[i]) %>% select(any_of(analysis_vars))
+#   
+#   # Appended to target DF
+#   corpus <- rbind(corpus, current)
+# }
+# 
+# rm(current) # Remove last instance 
 
 #@Sina: here is a faster and cleaner alternative:
 #however, the resulting data is far too large for my laptop to keep it in the memory
@@ -57,15 +93,41 @@ rm(current) # Remove last instance
 #each rds. otherwise it takes a long time.
 
 #########Alternative data reading script#############
-#rdsdataDIR<- paste0(getwd(),"/Data/rds/")
-#rds_list<- list.files(path = rdsdataDIR,pattern = "*.RDS",full.names = T)
-#tweet_corpus <-  lapply(rds_list,readRDS)
-#However, each file needs to be same dimensions, twitter academic API rds files are not
-#I am going to fix it
+
+twitter_rds_reader<- function(file_path, analysis_vars){
+  data<-readRDS(file_path)
+  #choose the important variables and make sure that everything is the same class
+  data <- data %>%
+    select(any_of(analysis_vars)) %>%
+    mutate(across(everything(),~as.character(.x)))
+  
+  #recode nas in binaries
+  na_vars<- Hmisc::contents(object = data)[[1]] %>%
+    as_tibble(rownames = "var_names") %>% 
+    filter(NAs > 0) %>% 
+    pull(var_names) %>%
+    grep(pattern = "is_*",x = .,perl = T,value = T)
+  
+  data<-data %>% mutate(across(.cols = all_of(na_vars), ~replace_na(.x,0))) %>%
+    mutate(tweet_date = lubridate::as_date(.$tweet_created_at)) %>% 
+    mutate(tweet_year = lubridate::floor_date(tweet_date,unit = "year"),
+           tweet_month = lubridate::floor_date(tweet_date, unit = "month"),
+           tweet_day = lubridate::floor_date(tweet_date, unit = "day"),
+           tweet_calendar_date = lubridate::wday(tweet_date,label = T))
+  return(data)
+}
+
+tweet_corpus <-  map_dfr(.x = files,.f = twitter_rds_reader,analysis_vars = analysis_vars)
+#save the data
+saveRDS(tweet_corpus,file = "C:/Users/sinaf/OneDrive - NTNU/Projects/communication_quality_repo/data/corpii/IO_corpus.RDS")
+
 #####################################################
 
 
 #<<<<<<< HEAD
+
+# ChRauh descriptives -----------------------------------------------------
+
 
 # Look at some descriptives ####
 
@@ -79,12 +141,20 @@ vars <- vars %>%
   rename(nas = V1) %>% 
   mutate(na_share = nas/nrow(corpus))
 
+#here is a a lazy alternative:
+# 
+# corpus %>%group_by(user_screenname) %>% 
+#   Hmisc::contents(.)[[1]] %>%
+#   as_tibble(rownames = "var_names") %>%
+#   mutate(na_share = (.$Nas/nrow(corpus)))
+
 =======
 # Look at some descriptives ####
 
 #>>>>>>> 54109f0f2826d999a0239385c2a3af8e4210620c
 # Accounts
 # 117
+#Now 115 because 2 agencies changed twitter handle
 length(unique(corpus$screen_name))
 
 # Language
@@ -97,8 +167,8 @@ table(corpus$lang, useNA = "ifany")
 #
 table(is.na(corpus$symbols))
 symbols <- corpus %>% filter(!is.na(symbols))
-<<<<<<< HEAD# What are these?
-=======#and these?
+<<<<<<< HEAD#Sina: What are these?
+=======#Sina:and these?
 # Drop var for now
 corpus$symbols <- NULL
 >>>>>>> 54109f0f2826d999a0239385c2a3af8e4210620c
@@ -240,4 +310,5 @@ corpus$source <- corpus$source %>%
 
 
 # Export corpus ####
-write_rds(corpus, "./tweetcorpora/EUtweets.RDS")
+#change the name based on the dataset
+write_rds(corpus, paste0(getwd(),"/data/corpii/IO_corpus"))
