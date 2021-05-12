561297937b62da288bdc4d9d61678511fdb2c639
SinaOzdemir <sina.ozdemir@ntnu.no>
2021-05-12 09:14:03 +0200

    rerun on historical data, needed to change variable names

--- Scripts/Analyses/0_BuildEUtweetCorpus_SFO.R
+++ Scripts/Analyses/0_BuildEUtweetCorpus_SFO.R
@@ -22,105 +22,13 @@ library(Hmisc)
 #Yep, 117 accounts because the Council of the EU and European Council merged their twitter accounts
 #now 115 accounts...
 subject_matter<-c("EU","IO","UK","TWT")
-data.path<- paste0(getwd(),"/data/",subject_matter[2],"/rds_clean/")
+data.path<- paste0(getwd(),"/data/corpii/")
 
 files <- list.files(path = data.path, pattern = "*.RDS",full.names = T)
 
 # Joint corpus ####
 
-# short cut ---------------------------------------------------------------
-
-#get a sample data to select necessary columns
-
-sample_data<-readRDS(files[9])
-#there in total 49 variables in a df
-#Not all of them are necessary,
-#eleminating them early on make it easier
-#Otherwise the data is too big to read into memory
-analysis_vars<- c("tweet_id",
-                  "is_retweet",
-                  "retweet_status_id",
-                  "is_reply",
-                  "reply_to_status_id",
-                  "is_quote",
-                  "quoted_status_id",
-                  "contains_media",
-                  "tweet_mentioned_username",
-                  "tweet_hashtags",
-                  "tweet_created_at",
-                  "tweet_text",
-                  "tweet_lang",
-                  "tweet_public_metrics.retweet_count",
-                  "tweet_public_metrics.reply_count",
-                  "tweet_public_metrics.like_count",
-                  "tweet_public_metrics.quote_count",
-                  "author_id",
-                  "user_username",
-                  "user_name",
-                  "user_description",
-                  "user_created_at",
-                  "user_public_metrics.followers_count",
-                  "user_public_metrics.following_count",
-                  "user_public_metrics.tweet_count",
-                  "user_public_metrics.listed_count")
-
-
-
-
-# All files should be identically structured
-
-# Target DF
-# corpus <- data.frame()
-# 
-# 
-# for (i in 1:length(files)) {
-#   
-#   # Progress
-#   print(i)
-#   
-#   # Read file
-#   current <- read_rds(files[i]) %>% select(any_of(analysis_vars))
-#   
-#   # Appended to target DF
-#   corpus <- rbind(corpus, current)
-# }
-# 
-# rm(current) # Remove last instance 
-
-#@Sina: here is a faster and cleaner alternative:
-#however, the resulting data is far too large for my laptop to keep it in the memory
-#my preferred method of analysis is to write a function and apply it individually to 
-#each rds. otherwise it takes a long time.
-
-#########Alternative data reading script#############
-
-twitter_rds_reader<- function(file_path, analysis_vars){
-  data<-readRDS(file_path)
-  #choose the important variables and make sure that everything is the same class
-  data <- data %>%
-    select(any_of(analysis_vars)) %>%
-    mutate(across(everything(),~as.character(.x)))
-  
-  #recode nas in binaries
-  na_vars<- Hmisc::contents(object = data)[[1]] %>%
-    as_tibble(rownames = "var_names") %>% 
-    filter(NAs > 0) %>% 
-    pull(var_names) %>%
-    grep(pattern = "is_*",x = .,perl = T,value = T)
-  
-  data<-data %>% mutate(across(.cols = all_of(na_vars), ~replace_na(.x,0))) %>%
-    mutate(tweet_date = lubridate::as_date(.$tweet_created_at)) %>% 
-    mutate(tweet_year = lubridate::floor_date(tweet_date,unit = "year"),
-           tweet_month = lubridate::floor_date(tweet_date, unit = "month"),
-           tweet_day = lubridate::floor_date(tweet_date, unit = "day"),
-           tweet_calendar_date = lubridate::wday(tweet_date,label = T))
-  return(data)
-}
-
-tweet_corpus <-  map_dfr(.x = files,.f = twitter_rds_reader,analysis_vars = analysis_vars)
-#save the data
-saveRDS(tweet_corpus,file = "C:/Users/sinaf/OneDrive - NTNU/Projects/communication_quality_repo/data/corpii/IO_corpus.RDS")
-
+corpus<- readRDS(paste0(data.path,"IO_corpus.RDS"))
 #####################################################
 
 
