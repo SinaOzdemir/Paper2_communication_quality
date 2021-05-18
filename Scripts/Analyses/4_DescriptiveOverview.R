0ea8ebaf8b8f75aa8ad8881fdd80d7fad3c20f9d
ChRauh <christian.rauh@gmail.com>
2021-05-18 16:20:08 +0200

    Build corpora by function (executed for EU & IO)

--- Scripts/Analyses/4_DescriptiveOverview.R
+++ Scripts/Analyses/4_DescriptiveOverview.R
@@ -225,3 +225,70 @@ pl.engagement <-
                                                           size = 14, face = "bold")))
 
 ggsave("./plots/DescriptiveOverview/Engagement.png", width = 20, height = 15, units = "cm")
+
+
+
+
+
+# # Analyse daily tweet volume ####
+# 
+# # Daily number of tweets in sample
+# days <- corpus %>% 
+#   mutate(day = str_trim(str_extract(created_at, ".*^? "))) %>% 
+#   select(day) %>% 
+#   group_by(day) %>% 
+#   summarise(count = n())
+# 
+# # Descriptives
+# min(days$day)  
+# max(days$day)
+# mean(days$count)
+# 
+# # Time series
+# dates <- seq(as.Date(min(days$day)), as.Date(max(days$day)), by = "days") %>% # all dates within range
+#   as.data.frame() %>% 
+#   rename(day = 1) %>% 
+#   mutate(day = as.character(day))
+# 
+# dates <- left_join(dates, days, by = "day")
+# dates$count[is.na(dates$count)] <- 0
+# 
+# breaks <- dates %>%  # Breaks for x-axis
+#   filter(str_detect(day, "01-01$")) %>% # 1st January
+#   select(day)
+# breaks <- as.character(breaks[,1])
+# labels <- str_remove(breaks, "-.*$") %>% unique() # Year only
+# 
+# ggplot(dates, aes(x= day, y= count)) + 
+#   geom_col()+
+#   scale_x_discrete(breaks = breaks, labels = labels)+
+#   labs(title = "Daily volume of tweets in EUtweet sample",
+#        subtitle = "117 verified accounts of supranational persons, offices, and institutions",
+#        x = "\nDay",
+#        y = "Number of tweets\n")+
+#   theme_bw()+
+#   theme()
+# 
+# ggsave("./plots/TweetVolumeDaily.png", width = 16, height = 10, units = "cm")
+# 
+# # Weekday
+# old.setting <- Sys.getlocale("LC_TIME") # Store current time setting of locale
+# Sys.setlocale("LC_TIME","English_United States.1252") # switch to English
+# wdays <- dates %>% 
+#   mutate(weekday = weekdays(as.Date(day))) %>% 
+#   group_by(weekday) %>% 
+#   summarise(mean = mean(count))
+# Sys.setlocale("LC_TIME", old.setting) # Reinstate old LC Time setting
+# wdays$weekday <- factor(wdays$weekday, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+# 
+# ggplot(wdays, aes(x= mean, y= fct_rev(weekday))) + 
+#   geom_col()+
+#   labs(title = "Tweets per weekday in EUtweet sample",
+#        subtitle = "117 verified accounts of supranational persons, offices, and institutions",
+#        x = "\nAverage number of tweets\n(2009-03-13/2021-03-14) ",
+#        y = "")+
+#   theme_bw()+
+#   theme()
+# 
+# ggsave("./plots/TweetVolumeWeekday.png", width = 16, height = 10, units = "cm")
+
