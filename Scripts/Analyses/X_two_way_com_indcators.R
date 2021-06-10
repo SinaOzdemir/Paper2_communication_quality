981b278a485740a4b4a4a57a0fbb543616600bd9
SinaOzdemir <sina.ozdemir@ntnu.no>
2021-06-10 18:55:17 +0200

    descriptive graphs are done, network functions are coming

--- Scripts/Analyses/X_two_way_com_indcators.R
+++ Scripts/Analyses/X_two_way_com_indcators.R
@@ -1,103 +1,48 @@
 #interactivity indicators
 
 # setup -------------------------------------------------------------------
+source(file = file.path(getwd(),"Scripts","Auxilary_scripts","two_way_interaction_analysis_functions.R"))
+data.path<-file.path(getwd(),"data")
 
-library(tidyverse)
-library(Hmisc)
-library(igraph)
-library(ggraph)
-data.path<-paste0(getwd(),"/data/corpii")
-graph.path<- paste0(getwd(),"/plots/")
-data.eu<-readRDS(file = paste0(data.path,"/EUcorpus_cleaned.RDS"))
-meta.data.eu<-
-#lets get a quick data summary-------------
+case_data<-data_reader(file_path = data.path,case = "EU")
 
 
-#this should be empty now:
-na_vars<- data_summary %>%
-  filter(NAs >0) %>% pull(var_names) %>%
-  grep(pattern = "is_*",ignore.case = T,perl = T,value = T,x = .)
-#replace na's in interaction indicators
+# data cleaning -----------------------------------------------------------
 
-sample_data_reduced<- sample_data_reduced %>% mutate(across(.cols = all_of(na_vars), ~replace_na(.x,0))) %>%
-  mutate(tweet_date = lubridate::as_date(.$tweet_created_at)) %>%
-  mutate(tweet_year = lubridate::floor_date(tweet_date,unit = "year"))
+case_data_clean<- data_cleaner(data = case_data)
 
-#descriptive data eu:
-yearly_volume <- data.eu %>% group_by(tweet_year) %>% tally()
+# descriptive interactivity -----------------------------------------------
 
-#all columns are saved as character to ensure homogeneity in earlier processes
-#now we need a few things as numeric
+eu_scatter_plot_reply<- descriptive_plots(data = case_data_clean,
+                                    case = "the EU",
+                                    what = "reply",
+                                    unit = "year",
+                                    graph_type = "scatter")
 
-data.eu<- data.eu %>% mutate(across(matches("is_*"),~as.numeric(.x)))
+eu_scatter_plot_quote<- descriptive_plots(data = case_data_clean,
+                                          case = "the EU",
+                                          what = "quote",
+                                          unit = "year",
+                                          graph_type = "scatter")
 
-eu_interactions_summary<- data.eu %>%
-  group_by(tweet_year) %>%
-  summarise(yearly_retweet_share = (sum(is_retweet)),
-            yearly_quote_share = (sum(is_quote)),
-            yearly_reply_share = (sum(is_reply))) %>%
-  left_join(x = .,y = yearly_volume, by = "tweet_year") %>% 
-  mutate_at(vars(c("yearly_retweet_share","yearly_quote_share","yearly_reply_share")), ~(./n)) %>%
-  pivot_longer(cols = starts_with("yearly_"),names_to = "variable",values_to = "share")
-
-yearly_interactivity<- eu_interactions_summary %>%
-  ggplot(data=.,aes(x = tweet_year, y = share)) +
-  geom_bar(aes(fill = share),stat = "identity",position = "dodge")+theme_minimal()+
-  facet_wrap(~variable)+
-  labs(x = "Year",y = "Share",title = "EU verified accounts interactivity rate",subtitle = "N accounts = 115 \n N tweets = 1065203")
-
-ggsave(filename = "yearly_interaction_rate.jpeg",plot = yearly_interactivity,path = graph.path)
-
-######between accounts comparison of interactivity######
-eu_acc_vol <- data.eu %>% group_by(user_name) %>% tally(name = "tweet_n")
-
-eu_intr_comp <- data.eu %>%
-  group_by(user_name) %>%
-  summarise(yearly_retweet_share = (sum(is_retweet)),
-            yearly_quote_share = (sum(is_quote)),
-            yearly_reply_share = (sum(is_reply))) %>% 
-  right_join(x = .,y = eu_acc_vol, by = "user_name") %>% 
-  mutate_at(vars(c("yearly_retweet_share","yearly_quote_share","yearly_reply_share")),~(./tweet_n)) 
-#####retweet#####
-eu_intr_retweet_p<- eu_intr_comp %>% ggplot(aes(x= user_name,y =yearly_retweet_share))+
-  geom_point(aes(color = yearly_retweet_share))+
-  theme_minimal()+geom_text(aes(label = user_name),hjust = 0, vjust = 0)+
-  scale_x_discrete(labels = NULL, breaks = NULL) +
-  theme_minimal()+
-  labs(x = "",y = "Share", title = "Retweet of EU accounts",subtitle = "Normalized by total number of tweets")
-#It feels like I am looking at almost perfect RNG...
-
-ggsave(filename = "inter_account_retweet.jpeg",plot = eu_intr_retweet_p,path = graph.path,units = "cm",width = 20,height = 15)
-#this looks ugly as hell in export, need to re-adjust dimensions
-
-####reply########
-eu_intr_reply_p<- eu_intr_comp %>% ggplot(aes(x= user_name,y =yearly_reply_share))+
-  geom_point(aes(color = yearly_reply_share))+
-  theme_minimal()+geom_text(aes(label = user_name),hjust = 0, vjust = 0)+
-  scale_x_discrete(labels = NULL, breaks = NULL) +
-  theme_minimal()+
-  labs(x = "",y = "Share", title = "Reply by EU accounts",subtitle = "Normalized by total number of tweets")
-#so they are not big on replying except for EU trade and Commissioner for aggriculture
-ggsave(filename = "inter_account_reply.jpeg",plot = eu_intr_reply_p,path = graph.path,units = "cm",width = 20,height = 15)
-
-#####quote#####
-
-eu_intr_quote_p <- eu_intr_comp %>% ggplot(aes(x= user_name,y =yearly_quote_share))+
-  geom_point(aes(color = yearly_quote_share))+
-  theme_minimal()+geom_text(aes(label = user_name),hjust = 0, vjust = 0)+
-  scale_x_discrete(labels = NULL, breaks = NULL) +
-  theme_minimal()+
-  labs(x = "",y = "Share", title = "Quotes by EU accounts",subtitle = "Normalized by total number of tweets")
-#they don't seem to be big on quoting in general either.
-ggsave(filename = "inter_account_quote.jpeg",plot = eu_intr_quote_p,path = graph.path,units = "cm",width = 20,height = 15)
+eu_scatter_plot_retweet<- descriptive_plots(data = case_data_clean,
+                                          case = "the EU",
+                                          what = "retweet",
+                                          unit = "year",
+                                          graph_type = "scatter")
 
 
-#would be great to look at who they reply, retweet and quote with a weighted network graph.
+(eu_scatter_plot_reply + plot_spacer() + eu_scatter_plot_quote) / (plot_spacer() + eu_scatter_plot_retweet + plot_spacer())
 
+# network graph of interactivity ------------------------------------------------
 
-# network graph of replies ------------------------------------------------
-#reply_to_user_id is missing in the clean data.
-#lets quickly add it
+test_data<- readRDS(file = list.files(file.path(getwd(),"data","EU","rds_clean"),pattern = "*.RDS",full.names = T)[1])
+
+#reply source:
+
+reply_data<- test_data %>% filter(is_reply == 1 & tweet_in_reply_to_user_id != author_id) %>%
+  select(tweet_id,author_id,user_username,tweet_in_reply_to_user_id)
+
 
 eu.data.path<- "C:/Users/sinaf/OneDrive - NTNU/Projects/communication_quality_repo/data/EU/rds/"
 eu.data.list<- list.files(path = eu.data.path, pattern = "*.RDS",full.names = T)
--- Scripts/Auxilary_scripts/two_way_interaction_analysis_functions.R
+++ Scripts/Auxilary_scripts/two_way_interaction_analysis_functions.R
@@ -1,13 +1,29 @@
 #two-way interaction indicator functions:
 
+print("Important: Functions are written based on data structure of a dataset in rds_clean in case folder!")
+
+req_colnames<-paste(c("tweet_id",
+                "author_id",
+                "user_username",
+                "is_quote",
+                "is_reply",
+                "is_retweet",
+                "reply_to_status_id",
+                "retweet_status_id",
+                "quoted_status_id",
+                "tweet_in_reply_to_user_id",
+                "tweet_date"),collapse =  " ")
+
+print(paste0("Functions require following columns: ",req_colnames))
+
 
 # read packages -----------------------------------------------------------
 
 
 
-packs<- c("tidyverse","Hmisc","rtweet","igraph","ggraph")
+packs<- c("tidyverse","Hmisc","rtweet","ggraph","patchwork")
 existing_packs<-installed.packages()[,1]
-
+#Function works
 package_loader<- function(packs = character(0)){
   existing_packs<- installed.packages()[,1]
   if(all(packs%in%existing_packs)){
@@ -24,6 +40,7 @@ package_loader(packs = packs)
 
 # helper functions --------------------------------------------------------
 
+#Function works!
 make.dir<- function(file.path){
   if(dir.exists(file.path)){
     return(file.path)
@@ -33,24 +50,180 @@ make.dir<- function(file.path){
   }
 }
 
+#Function works!
+data_reader<- function(file_path, case = c("UK","EU","IO")){
+  
+  req_columns<- c("tweet_id",
+                      "author_id",
+                      "user_username",
+                      "is_quote",
+                      "is_reply",
+                      "is_retweet",
+                      "reply_to_status_id",
+                      "retweet_status_id",
+                      "quoted_status_id",
+                      "tweet_in_reply_to_user_id",
+                      "tweet_date")
+  
+  check<- readline(prompt = "This function is intended to read final analysis data.\n Are you reading the preprocessed data? \n 1: Yes\n 2: No")
+  
+  if(check == 1){
+  
+  cat("reading the preprocessed data.","Due to file size this may take a minute.",sep = "\n")
+    
+  if(case == "UK"){
+    data_path<- file.path(file_path,"corpii","UK_corpus.RDS")
+  }else if(case == "IO"){
+    data_path <- file.path(file_path,"corpii","IO_corpus.RDS")
+  }else if(case == "EU"){
+    data_path <- file.path(file_path,"corpii","EU_analysis_data.RDS")
+  }else{
+    stop("please choose one of the following cases:\n UK\n EU \n IO")
+  }
+    
+  data<- readRDS(data_path)
+
+  if(all(req_columns%in%colnames(data))){
+    cat("all required columns are present!")
+    return(data)
+  }
+  else{
+    missing_columns<- req_columns[-which(req_columns%in%colnames(data))]
+    cat("following columns are missing:", missing_columns, sep = "\n")
+    stop("aborting the data read")
+  }
+  
+  }
+  else{
+    stop("you need to import the preprocessed data for other functions to work")
+  }
+}
 
 # variable manipulators ---------------------------------------------------
 
+#Function works!
 data_cleaner<- function(data){
   
   clean_data<- data %>%filter(langlang %in% "en") %>% 
     mutate(across(.cols = starts_with("is_"), ~tidyr::replace_na(.x, 0))) %>%
     mutate(across(.cols = starts_with("is_"),~as.numeric(.x))) %>% 
     mutate(tweet_date = lubridate::as_date(.$tweet_created_at)) %>%
-    mutate(tweet_year = lubridate::floor_date(tweet_date,unit = "year"),
-           tweet_month = lubridate::floor_date(tweet_date,unit = "month"),
+    mutate(tweet_year = lubridate::year(lubridate::floor_date(tweet_date,unit = "year")),
+           tweet_month = lubridate::month(lubridate::floor_date(tweet_date,unit = "month"),label = T),
            tweet_week = lubridate::floor_date(tweet_date,unit = "week"),
-           tweet_day = lubridate::floor_date(tweet_date,unit = "day"),
-           tweet_calendar_day = weekdays(tweet_date))
+           tweet_calendar_day = base::weekdays(tweet_date))
   
   return(clean_data)
     
 }
 
+#Function works
+descriptive_plots<- function(data,
+                             case = c("the EU","the UK","IO"),
+                             what = c("reply","quote","retweet"),
+                             unit = c("year","month","week","calendar_day"),
+                             graph_type = c("bar_stack","bar_dodge","scatter")){
+  
+  trimmed_data<- data %>% select(tweet_id,
+                                 author_id,
+                                 user_username,
+                                 tweet_in_reply_to_user_id,
+                                 matches("is_"),
+                                 tweet_date,
+                                 tweet_year,
+                                 tweet_month,
+                                 tweet_calendar_day)
+  
+  if(what == "reply"){
+    cat("removing threads pretending to be replies")
+    trimmed_data<- trimmed_data %>% 
+      filter(tweet_in_reply_to_user_id != author_id)
+  }
+  
+  aggregation_data<- trimmed_data %>%
+    select(tweet_id,author_id,user_username, matches(what),matches(unit))
+  
+  colnames(aggregation_data)<-c("tweet_id","user_id","screen_name","filtering_unit","aggregation_unit")
+  
+  #reading from github didn't work, read the local file.Right now it only works for the EU, need to make 
+  #manual data reading more flexible to accommodate other cases.
+  
+  
+  # TODO: There are some missing accounts in the manually coded categories. It should be added there at some point
+  manual_categories<-readRDS(file = file.path(getwd(),"data","Accounts","EU_handcoded_categories.RDS"))
+  
+  
+  aggregation_data<- manual_categories %>%
+    filter(user_id%in%aggregation_data$user_id & screen_name %in%aggregation_data$screen_name) %>% 
+    select(user_id,screen_name,Actor_type) %>% left_join(x = aggregation_data,y = .,by = c("user_id","screen_name"))
+  
 
-descriptive_plots<- function(data,what = c("reply","quote","retweet"),unit = c("year","month"))
+  aggregated_data<- aggregation_data %>%
+    group_by(aggregation_unit,Actor_type) %>% 
+    summarise(absolute_freq = sum(filtering_unit),
+              percentage_share = (sum(filtering_unit)/n()))
+  
+  if(graph_type == "bar_stack"){
+  desc_plot<- aggregated_data %>%drop_na() %>% 
+    ggplot(.,aes(x = aggregation_unit,y = percentage_share)) +
+    geom_bar(aes(fill = Actor_type),color = "black",position = "stack",stat = "identity")+
+    scale_x_continuous(breaks = min(aggregated_data$aggregation_unit):max(aggregated_data$aggregation_unit))+
+    #ylim(0,1)+
+    # geom_text(aes(label = absolute_freq),
+    #           color = "white",
+    #           size = 5,
+    #           fontface = "bold",
+    #           position = position_stack(vjust = .5))+
+    theme_minimal()+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+    labs(x = unit,
+         y = "Percentage share",
+         title = paste0(case," verified accounts ", what," share"),
+         subtitle = paste0("N accounts = ",
+                           length(unique(aggregation_data[,"screen_name"])),
+                           "\n",
+                           "N tweets =", nrow(aggregation_data)))
+    
+  return(desc_plot)}
+  
+  if(graph_type == "bar_dodge"){
+    desc_plot<- aggregated_data %>%drop_na() %>% 
+      ggplot(.,aes(x = aggregation_unit,y = percentage_share)) +
+      geom_bar(aes(fill = Actor_type),color = "black",position = "dodge",stat = "identity")+
+      scale_x_continuous(breaks = min(aggregated_data$aggregation_unit):max(aggregated_data$aggregation_unit))+
+      #ylim(0,1)+
+      # geom_text(aes(label = absolute_freq),
+      #           color = "white",
+      #           size = 5,
+      #           fontface = "bold",
+      #           position = position_stack(vjust = .5))+
+      theme_minimal()+
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+      labs(x = unit,
+           y = "Percentage share",
+           title = paste0(case," verified accounts ", what," share"),
+           subtitle = paste0("N accounts = ",
+                             length(unique(aggregation_data[,"screen_name"])),
+                             "\n",
+                             "N tweets =", nrow(aggregation_data)))
+    
+    return(desc_plot)}
+  
+  if(graph_type == "scatter"){
+    warning("!!Scatter plot is still very primitive!!")
+    
+    desc_plot<- aggregated_data %>%drop_na() %>% 
+      ggplot(.,aes(x = aggregation_unit,y = percentage_share)) +
+      geom_point(aes(color = Actor_type,size = percentage_share ,alpha = percentage_share))+
+      scale_x_continuous(breaks = min(aggregated_data$aggregation_unit):max(aggregated_data$aggregation_unit))+
+      theme_minimal()+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+      labs(x = unit,
+           y = "Percentage share",
+           title = paste0(case," verified accounts ", what," share"),
+           subtitle = paste0("N accounts = ",
+                             length(unique(aggregation_data[,"screen_name"])),
+                             "\n","N tweets =",
+                             nrow(aggregation_data)))
+    return(desc_plot)
+  }
+}
